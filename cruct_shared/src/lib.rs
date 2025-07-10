bf57865018a23a5ec73539b0604289cb177decb4
Chiko <chiko@envs.net>
2025-07-10 20:36:39 +0100

    fix: i don't even know when i tried to export this parser

--- cruct_shared/src/lib.rs
+++ cruct_shared/src/lib.rs
@@ -1,13 +1,5 @@
 pub mod parser;
 pub mod source;
 
-pub use parser::{
-    ConfigValue,
-    FileFormat,
-    FromConfigValue,
-    Parser,
-    ParserError,
-    YmlParser,
-    get_parser,
-};
+pub use parser::{ConfigValue, FileFormat, FromConfigValue, Parser, ParserError, get_parser};
 pub use source::{CliSource, ConfigBuilder, ConfigFileSource, ConfigSource};
